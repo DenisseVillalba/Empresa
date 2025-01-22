250125140cc1437ff30adcd4921e245557c3464e
DenisseVillalba <denissevillalbaproa@gmail.com>
2025-01-22 07:57:22 -0500

    Add files via upload

---
+++ SimulacionNavegacionCRUD/src/simulacionnavegacioncrud/SNavegacionCRUD.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simulacionnavegacioncrud;
+
+import vista.MainFrame;
+/**
+ *
+ * @author NODELI
+ */
+public class SNavegacionCRUD {
+    public class SimulacionNavegacionCRUD {
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
+}
